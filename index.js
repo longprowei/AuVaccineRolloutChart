3886dcb30034bd331bb6f97b09e27ab3d3c5c7e1
Chenglong <longprowei@gmail.com>
2021-05-17 17:34:26 +1000

    add seven days average data

--- index.js
+++ index.js
@@ -11,20 +11,39 @@ const url = "https://vaccinedata.covid19nearme.com.au/data/all.json";
             let labels = [];
             let nswDayData = [];
             let nswTotalData = [];
+            let dayDataSevenAvg = [];
+            let nswDayDataSevenAvg = [];
 
             data.forEach(element => {
                 labels.push(element.DATE_AS_AT);
                 dayData.push(element.TOTALS_NATIONAL_LAST_24HR);
                 totalData.push(element.TOTALS_NATIONAL_TOTAL);
-                nswDayData.push(element.STATE_CLINICS_NSW_LAST_24HR + element.CWTH_AGED_CARE_NSW_LAST_24HR + element.CWTH_PRIMARY_CARE_NSW_LAST_24HR);
-                nswTotalData.push(element.STATE_CLINICS_NSW_TOTAL + element.CWTH_AGED_CARE_NSW_TOTAL + element.CWTH_PRIMARY_CARE_NSW_TOTAL);
+                nswDayData.push(element.STATE_CLINICS_NSW_LAST_24HR ? element.STATE_CLINICS_NSW_LAST_24HR : 0 + element.CWTH_AGED_CARE_NSW_LAST_24HR ? element.CWTH_AGED_CARE_NSW_LAST_24HR : 0 + element.CWTH_PRIMARY_CARE_NSW_LAST_24HR ? element.CWTH_PRIMARY_CARE_NSW_LAST_24HR : 0);
+                nswTotalData.push(element.STATE_CLINICS_NSW_TOTAL ? element.STATE_CLINICS_NSW_TOTAL : 0  + element.CWTH_AGED_CARE_NSW_TOTAL ? element.CWTH_AGED_CARE_NSW_TOTAL : 0 + element.CWTH_PRIMARY_CARE_NSW_TOTAL ? element.CWTH_PRIMARY_CARE_NSW_TOTAL : 0);
             });
-            createGraph(dayData, totalData, labels, 'myChart1', 'rgba(0, 0, 255, 0.3)');
-            createGraph(nswDayData, nswTotalData, labels, 'myChart2', 'rgba(0, 255, 0, 0.3)');
+
+
+            for (let i = 0; i < dayData.length; i++) {
+                let currIndex = i;
+                let count = 0;
+                let sumAu = 0;
+                let sumNsw = 0;
+                while (i - currIndex <= 6 && currIndex >= 0) {
+                    sumAu += dayData[currIndex];
+                    sumNsw += nswDayData[currIndex];
+                    currIndex--;
+                    count++;
+                }
+                dayDataSevenAvg.push(count < 6 ? undefined : Math.floor(sumAu / count));
+                nswDayDataSevenAvg.push(count < 6 ? undefined : Math.floor(sumNsw / count));
+            }
+
+            createGraph(dayData, totalData, dayDataSevenAvg, labels, 'myChart1', 'rgba(0, 0, 255, 0.3)');
+            createGraph(nswDayData, nswTotalData, nswDayDataSevenAvg, labels, 'myChart2', 'rgba(0, 255, 0, 0.3)');
         });
 })();
 
-function createGraph(dayData, totalData, labels, chartId, barChartBg) {
+function createGraph(dayData, totalData, sevenDayData, labels, chartId, barChartBg) {
     let ctx = document.getElementById(chartId).getContext('2d');
     new Chart(ctx, {
         data: {
@@ -32,7 +51,7 @@ function createGraph(dayData, totalData, labels, chartId, barChartBg) {
                 type: 'bar',
                 label: 'Everyday rollout',
                 data: dayData,
-                order: 1,
+                order: 2,
                 yAxisID: 'y',
                 backgroundColor: barChartBg,
             }, {
@@ -42,6 +61,13 @@ function createGraph(dayData, totalData, labels, chartId, barChartBg) {
                 yAxisID: 'y1',
                 order: 0,
                 backgroundColor: 'red',
+            }, {
+                type: 'line',
+                label: '7 Days Average',
+                data: sevenDayData,
+                yAxisID: 'y',
+                order: 1,
+                backgroundColor: 'blue',
             }],
             labels: labels
         },
